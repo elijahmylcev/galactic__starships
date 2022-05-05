958d4745872b0fd856cd045defeff186dde79c56
Elijah Mylcev <elijahmylcev@gmail.com>
2022-05-05 19:09:23 +0300

    clear && swapi-service file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true
   },
   extends: [
     'plugin:vue/vue3-essential',
@@ -13,5 +14,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quotes': ['error', 'single'],
+    'no-underscore-dangle': 0,
+    'linebreak-style': 0,
+    'class-methods-use-this': 0,
+    'no-return-await': 0
   },
 };
---
+++ src/services/swapi-service.js
@@ -0,0 +1,45 @@
+export default class SwapiService {
+  _apiBase = 'https://swapi.dev/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} --> received ${res.status}`);
+    }
+    return await res.json();
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource('/starships/');
+    return res.results.map(this._transformStarship);
+  }
+
+  async getStarship(id) {
+    const starship = await this.getResource(`/starships/${id}/`);
+    return this._transformStarship(starship);
+  }
+
+  // Функция получает id из объекта, приходящего с api
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  // Трансформирую данные, чтобы интегрировать их в компонент
+  _transformStarship = (starship) => {
+    const id = this._extractId(starship);
+    return {
+      id,
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCopacity: starship.cargoCopacity,
+      adress: `https://starwars-visualguide.com/assets/img/starships/${id}.jpg`,
+    };
+  };
+}
